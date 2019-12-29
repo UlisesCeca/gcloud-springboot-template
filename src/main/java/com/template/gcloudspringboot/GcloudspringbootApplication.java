b631feb5611874865d38fa421c3cf6284cafc4b7
Ulises <ulises@ulisesceca.com>
2019-12-29 14:52:18 +0100

    spring sec

--- src/main/java/com/template/gcloudspringboot/GcloudspringbootApplication.java
+++ src/main/java/com/template/gcloudspringboot/GcloudspringbootApplication.java
@@ -2,11 +2,13 @@ package com.template.gcloudspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class GcloudspringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GcloudspringbootApplication.class, args);
