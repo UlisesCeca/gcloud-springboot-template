e12e5eb414dd5bdbc43ab5bfee677de8f71630d1
Ulises <ulises@ulisesceca.com>
2019-12-29 01:49:51 +0100

    first commit

---
+++ src/test/java/com/template/gcloudspringboot/GcloudspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.gcloudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcloudspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
